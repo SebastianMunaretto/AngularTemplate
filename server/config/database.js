20f004ec8d94f2f297a6beaed01a71e1c92cb8e3
SebastianMunaretto <sebastianmunaretto@gmail.com>
2022-01-16 11:07:58 +0100

    Backup

---
+++ server/config/database.js
@@ -0,0 +1,7 @@
+const crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+    uri: "mongodb://localhost:27017/course-database",
+    secret: crypto,
+    db: 'course-database'
+}
